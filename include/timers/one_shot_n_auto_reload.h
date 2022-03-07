6a1606df9533ba819901cb5507c0f916b06dcb41
snandasena <sajithgithub@gmail.com>
2022-03-07 21:03:13 +0530

    Improved

---
+++ include/timers/one_shot_n_auto_reload.h
@@ -0,0 +1,18 @@
+//
+// Created by sajith on 3/7/22.
+//
+
+#ifndef UNTITLED10_ONE_SHOT_N_AUTO_RELOAD_H
+#define UNTITLED10_ONE_SHOT_N_AUTO_RELOAD_H
+
+#include "base.h"
+
+
+
+void SetUp()
+{
+
+}
+
+
+#endif //UNTITLED10_ONE_SHOT_N_AUTO_RELOAD_H
--- src/myapp.cpp
+++ src/myapp.cpp
@@ -1,8 +1,9 @@
-#include "queue_management/queue_sets.h"
+#include "timers/one_shot_n_auto_reload.h"
 
 void setup()
 {
     Serial.begin(9600);
+    pinMode(13, OUTPUT);
 // write your initialization code here
     SetUp();
 }
@@ -10,5 +11,9 @@ void setup()
 
 void loop()
 {
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
 // write your code here
 }
